75863491827783b550b549ed4edb9e26cd53248e
Jesse <C.Driskill@F5.com>
2015-03-20 20:08:28 -0700

    Corrected the frequency with which column headers are printed.

--- ltmStats-dev.pl
+++ ltmStats-dev.pl
@@ -302,16 +302,16 @@ do {
 
     if ($VERBOSE) {
       @winSize = &GetTerminalSize;
-      if ($iterations == 1 || !$iterations%$winSize[1]) {
+      if ($iterations == 1 || ($iterations%$winSize[1]) == 0 ) {
       printf("%7s% 6s% 6s% 10s% 6s% 8s% 8s% 9s% 9s% 9s% 9s% 9s% 9s\n", 
           "RunTime", "sCPU", "tCPU", "Mem (MB)", "cCPS", "sCPS", "HTTP", "cConns", "sConns", "In/Mbs", "Out/Mbs", "cPPS In", "cPPS Out");
       }
-      printf("%7.2f% 5.2f% 5.2f% 8d% 8d% 8d% 8d% 9d% 9d% 9d% 9d% 9d% 9d\n", 
+      printf("%7.2f% 6.2f% 6.2f% 8d% 8d% 8d% 8d% 9d% 9d% 9d% 9d% 9d% 9d\n", 
           @$out{qw/runTime cpuUtil tmmUtil memUsed cNewConns sNewConns httpReq cCurConns sCurConns cBitsIn cBitsOut cPktsIn cPktsOut/})
     }
     else {
       @winSize = &GetTerminalSize;
-      if ($iterations == 1 || !$iterations%$winSize[1]) {
+      if ($iterations == 1 || ($iterations%$winSize[1]) == 0 ) {
       printf("%7s% 7s% 10s% 6s% 8s% 8s% 9s% 9s% 9s% 9s\n", 
           "RunTime", "tCPU", "Mem (MB)", "cCPS", "sCPS", "HTTP", "cConns", "sConns", "In/Mbs", "Out/Mbs");
       }
